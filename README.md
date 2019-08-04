d879acdf16d4f5a81496e16f9adbe8d3b0428700
mycognosist <mycognosist@cryptolab.net>
2019-08-04 16:18:30 +0100

    Add working UDP server

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
+# esp8266_dht11_udp
 
+Connect to local WiFi network with an ESP8266 and respond to UDP requests on port 3210 with temperature and humidity readings from attached DHT11.
 
-Guide:
+ - Upload `esp8266_dht11_udp.ino` to your ESP8266  
+ - Open the serial terminal: `cu -l /dev/ttyUSB0 -s 115200`
+ - Send UDP packet with contents `GET` to ESP8255 IP and port (3210)
+ - Terminal output:
 
-Wiring (DHT11 to NodeMCU):
+     ```
+     Connected.
+     Received 3 bytes from 192.168.0.165, port 3210
+     UDP packet contents: GET
+     Sending UDP response with temperature and humidity: 17.0 60.00
+     ```
 
-VCC to 3.3V
-GND to GND
-SIG to D4
-
+Be sure to set your WiFi AP credentials in the code.
--- esp8266_dht11_udp.ino
+++ esp8266_dht11_udp.ino
@@ -1,18 +1,23 @@
 /* 
- * ESP8266 NodeMCU DHT11 - Humidity Temperature Sensor Example
+ * ESP8266 NodeMCU DHT11 UDP Server
  * 
  * References
  * https://circuits4you.com/2017/12/31/nodemcu-pinout/
  * https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
  *
  */
-        
-#include "DHTesp.h"
-#include "ESP8266WiFi.h"
+
+#include <DHTesp.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
 #define DHTpin 2    //D4 of NodeMCU is GPIO2
 
 DHTesp dht;
+
+WiFiUDP Udp;
+unsigned int localUdpPort = 3210;
+char incomingPacket[256];
 
 void setup()
 {
@@ -21,7 +26,7 @@ void setup()
     Serial.println("Initializing DHT11 sensor on D4 (GPIO2)");       
     dht.setup(DHTpin, DHTesp::DHT11);
 
-    WiFi.begin("Home", "0217890132");
+    WiFi.begin("Home", "an_actual_password");
 
     Serial.print("Connecting to Home WiFi AP");
     while (WiFi.status() != WL_CONNECTED)
@@ -34,25 +39,48 @@ void setup()
     Serial.print("Connected, IP address: ");
     Serial.println(WiFi.localIP());
     
-    Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
+    Serial.println("Activating UDP server");
+    Udp.begin(localUdpPort);
+    Serial.printf("UDP server active, port %d\n", localUdpPort);
+    Serial.println();
 }
 
 void loop()
 {
-    delay(dht.getMinimumSamplingPeriod());
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
+    {
+        Serial.printf("Received %d bytes from %s, port %d", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+        int len = Udp.read(incomingPacket, 255);
+        
+        if (len > 0)
+        {
+            incomingPacket[len] = 0;
+        }
+        Serial.println();
+        Serial.printf("UDP packet contents: %s", incomingPacket);
+        Serial.println();
 
-    float humidity = dht.getHumidity();
-    float temperature = dht.getTemperature();
+        delay(dht.getMinimumSamplingPeriod());
 
-    Serial.print(dht.getStatusString());
-    Serial.print("\t");
-    Serial.print(humidity, 1);
-    Serial.print("\t\t");
-    Serial.print(temperature, 1);
-    Serial.print("\t\t");
-    Serial.print(dht.toFahrenheit(temperature), 1);
-    Serial.print("\t\t");
-    Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
-    Serial.print("\t\t");
-    Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+        float humidity = dht.getHumidity();
+        float temperature = dht.getTemperature();
+
+        char temperature_str[16];
+        dtostrf(temperature, 3, 1, temperature_str);
+        
+        char humidity_str[16];
+        dtostrf(humidity, 4, 2, humidity_str);
+        
+        char replyPacket[32];
+        
+        sprintf(replyPacket, "%s %s", temperature_str, humidity_str);
+
+        Serial.print("Sending UDP response with temperature and humidity: ");
+        Serial.println(replyPacket);
+        
+        Udp.beginPacket(Udp.remoteIP(), 3210);
+        Udp.write(replyPacket);
+        Udp.endPacket();
+    }
 }
